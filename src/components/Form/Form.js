87b57ee7e5a86e930d606fb65ffc24e64512a08e
Sara <sarassk21@gmail.com>
2025-02-19 20:05:01 +0530

    "FIXED: phone number, scroll to right, skils images"

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,12 @@
-import "./FormStyles.css"
+import "./Form.css"
 import React from 'react'
-import { FaLinkedin,FaMailBulk ,FaInstagram,FaGithub,FaFacebook,FaTwitter } from "react-icons/fa"
+import { FaLinkedin, FaMailBulk, FaInstagram, FaGithub, FaFacebook, FaTwitter } from "react-icons/fa"
 // use rafce to create dummmy structure
 
 const Form = () => {
   return (
-    <div className="form">
-    <form>
+    <div className="form-container">
+      <form>
         <label>Your Sweet Name:</label>
         <input type="text"></input>
         <label>Email:</label>
@@ -20,15 +20,15 @@ const Form = () => {
         <button className="btn">Submit</button>
         <br></br><br></br>
         <div className="socials"><br></br>
-        <a href="https://www.linkedin.com/in/sara-shaikh/" target="_blank"><FaLinkedin size={50} style={{color:"#fff" ,marginRight:"4rem"}} /></a>
-        <a href="https://github.com/Sara-Shaikh-21/" target="_blank"><FaGithub size={50} style={{color:"#fff" ,marginRight:"4rem"}}/></a>
-        <a href="https://www.facebook.com/sara.shaikh.21/" target="_blank"><FaFacebook size={50} style={{color:"#fff" ,marginRight:"4rem"}}/></a>
-        <a href="https://twitter.com/sara_shaikh_21" target="_blank"><FaTwitter size={50} style={{color:"#fff" ,marginRight:"4rem"}}/></a>
-        <a href="mailto: sarassk21@gmail.com" target="_blank"><FaMailBulk size={50} style={{color:"#fff" ,marginRight:"0rem"}}/></a>
+          <a href="https://www.linkedin.com/in/sara-shaikh/" target="_blank"><FaLinkedin size={50} style={{ color: "#fff", marginRight: "4rem" }} /></a>
+          <a href="https://github.com/Sara-Shaikh-21/" target="_blank"><FaGithub size={50} style={{ color: "#fff", marginRight: "4rem" }} /></a>
+          <a href="https://www.facebook.com/sara.shaikh.21/" target="_blank"><FaFacebook size={50} style={{ color: "#fff", marginRight: "4rem" }} /></a>
+          <a href="https://twitter.com/sara_shaikh_21" target="_blank"><FaTwitter size={50} style={{ color: "#fff", marginRight: "4rem" }} /></a>
+          <a href="mailto: sarassk21@gmail.com" target="_blank"><FaMailBulk size={50} style={{ color: "#fff", marginRight: "0rem" }} /></a>
 
         </div>
-    </form>
-    
+      </form>
+
     </div>
   )
 }
