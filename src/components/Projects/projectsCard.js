4bb00e2af64af5c33acbfd53f5e335d0ab38d2d9
Sara <sarassk21@gmail.com>
2025-02-19 17:53:45 +0530

    "FIXED: Extra scroll to right"

--- src/components/Projects/projectsCard.js
+++ src/components/Projects/projectsCard.js
@@ -5,16 +5,16 @@ import { NavLink } from "react-router-dom";
 const ProjectsCard = (props) => {
   return (
     <div className="projects-card">
-            <img src={props.imgsrc} alt="image" />
-            <h2 className="project-title">{props.title}</h2>
-            <div className="pro-details">
-                <p>{props.text}</p>
-                <div className="pro-btns">
-                 <NavLink to={props.view} className="btn">View</NavLink>
-                 <NavLink to={props.source} className="btn">Source</NavLink>   
-                </div>
-            </div>
+      <img src={props.imgsrc} alt="image" />
+      <h2 className="project-title">{props.title}</h2>
+      <div className="pro-details">
+        <p>{props.text}</p>
+        <div className="pro-btns">
+          <NavLink to={props.view} className="btn">View</NavLink>
+          <NavLink to={props.source} className="btn">Source</NavLink>
         </div>
+      </div>
+    </div>
   )
 }
 
